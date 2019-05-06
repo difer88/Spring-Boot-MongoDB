29e90b1eb116cbeec421a073fa220b87e72a82ce
Diego Fernandes <diegofernandes88@hotmail.com>
2019-05-06 00:29:09 -0300

    Implementation of user insertion with POST

--- src/main/java/com/diegofernandes/springmongo/services/UserService.java
+++ src/main/java/com/diegofernandes/springmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.diegofernandes.springmongo.services;
 
 import com.diegofernandes.springmongo.domain.User;
+import com.diegofernandes.springmongo.dto.UserDTO;
 import com.diegofernandes.springmongo.repository.UserRepository;
 import com.diegofernandes.springmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 }
