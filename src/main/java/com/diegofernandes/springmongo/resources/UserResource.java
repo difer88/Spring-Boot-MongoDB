e32af9f8c6f77599de183ddd29f7c490ba808827
Diego Fernandes <diegofernandes88@hotmail.com>
2019-05-11 19:02:25 -0300

    Creation of the endpoint to return the posts of a user

--- src/main/java/com/diegofernandes/springmongo/resources/UserResource.java
+++ src/main/java/com/diegofernandes/springmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.diegofernandes.springmongo.resources;
 
+import com.diegofernandes.springmongo.domain.Post;
 import com.diegofernandes.springmongo.domain.User;
 import com.diegofernandes.springmongo.dto.UserDTO;
 import com.diegofernandes.springmongo.services.UserService;
@@ -65,6 +66,14 @@ public class UserResource {
 
         return  ResponseEntity.noContent().build();
 
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+
+        User obj = service.findById(id);
+
+        return  ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
