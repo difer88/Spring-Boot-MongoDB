dc48673991875ca26ccb018e53c10e29d7f3331c
Diego Fernandes <diegofernandes88@hotmail.com>
2019-05-11 18:50:59 -0300

    Referencing user posts

--- src/main/java/com/diegofernandes/springmongo/config/Instantiation.java
+++ src/main/java/com/diegofernandes/springmongo/config/Instantiation.java
@@ -48,13 +48,22 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("14/08/2019"), "Going to travel",
                 "I'm getting ready to travel to London !", new AuthorDTO(user1));
 
-        Post post2 = new Post(null, sdf.parse("26/10/2019"), "Disease prevention",
+        Post post2 = new Post(null, sdf.parse("19/08/2019"), "Travelling again",
+                "Now leaving London to go to Paris !", new AuthorDTO(user1));
+
+        Post post3 = new Post(null, sdf.parse("26/10/2019"), "Disease prevention",
                 "Always remember to wash your hands !", new AuthorDTO(user2));
 
-        Post post3 = new Post(null, sdf.parse("05/02/2019"), "Morning walk",
+        Post post4 = new Post(null, sdf.parse("05/02/2019"), "Morning walk",
                 "I'm going to walk in the park with my dog !", new AuthorDTO(user3));
 
-        postRepository.saveAll(Arrays.asList(post1, post2));
+        postRepository.saveAll(Arrays.asList(post1, post2, post3, post4));
+
+        user1.getPosts().addAll(Arrays.asList(post1, post2));
+        user2.getPosts().add(post3);
+        user3.getPosts().add(post4);
+
+        userRepository.saveAll(Arrays.asList(user1, user2, user3));
 
     }
 }
